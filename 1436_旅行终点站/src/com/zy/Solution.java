000e984fdcedaf163f7e64469c11f5541a1ec640
zy <zy@747.com>
2021-10-08 14:52:47 +0800

    1436_旅行终点站

---
+++ 1436_旅行终点站/src/com/zy/Solution.java
@@ -0,0 +1,49 @@
+package com.zy;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @author zhangyu
+ * @date 2021/10/1 21:12
+
+    给你一份旅游线路图，该线路图中的旅行线路用数组 paths 表示，其中 paths[i] = [cityAi, cityBi] 表示该线路将会从 cityAi 直接前往 cityBi 。请你找出这次旅行的终点站，即没有任何可以通往其他城市的线路的城市。
+
+    题目数据保证线路图会形成一条不存在循环的线路，因此恰有一个旅行终点站。
+
+    示例 1：
+
+    输入：paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+    输出："Sao Paulo"
+    解释：从 "London" 出发，最后抵达终点站 "Sao Paulo" 。本次旅行的路线是 "London" -> "New York" -> "Lima" -> "Sao Paulo" 。
+    示例 2：
+
+    输入：paths = [["B","C"],["D","B"],["C","A"]]
+    输出："A"
+    解释：所有可能的线路是：
+    "D" -> "B" -> "C" -> "A". 
+    "B" -> "C" -> "A". 
+    "C" -> "A". 
+    "A". 
+    显然，旅行终点站是 "A" 。
+
+
+ */
+public class Solution {
+
+    public String destCity(List<List<String>> paths) {
+        String res = null;
+        HashSet<String> outsets = new HashSet<>();
+        for(List path : paths){
+            outsets.add((String)path.get(0));  // 把所有的起点与途中经过的地点放入 set
+        }
+
+        for (List path : paths){
+            res = (String)path.get(1);
+            if(!outsets.contains(res)){    // 如果一个地点不在起点与途中经过的地点的 set 中 那么他就是终点
+                break;
+            }
+        }
+        return res;
+    }
+}
